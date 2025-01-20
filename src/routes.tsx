453756e01c1005261a1897f2b4743628469e4bc2
z0l1 <andzo1998@gmail.com>
2025-01-20 02:00:28 +0100

    Add redirect to projects

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,11 @@
-import { Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { ProjectsRoutes } from "./pages/Projects/routes";
 
 export const AppRoutes = () => {
-	return <Routes>{ProjectsRoutes}</Routes>;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigate replace to="/projects" />} />
+			{ProjectsRoutes}
+		</Routes>
+	);
 };
